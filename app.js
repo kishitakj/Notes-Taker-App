3fb59c4aab53b1a51a115be7c0219631d7b30a67
Kishita Jain <kishitakj139@gmail.com>
2021-04-20 23:48:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const chalk = require('chalk')
 const yargs = require('yargs')
 const notes = require('./notes.js')
 
-// Customize yargs version
+
 yargs.version('1.1.0')
 
-// Create add command
+
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
@@ -26,7 +26,7 @@ yargs.command({
     }
 })
 
-// Create remove command
+
 yargs.command({
     command: 'remove',
     describe: 'Remove a note',
@@ -42,7 +42,7 @@ yargs.command({
     }
 })
 
-// Create list command
+
 yargs.command({
     command: 'list',
     describe: 'List your notes',
@@ -51,7 +51,7 @@ yargs.command({
     }
 })
 
-// Create read command
+
 yargs.command({
     command: 'read',
     describe: 'Read a note',
